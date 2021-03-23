96151bf502bf9cb942de898464c0b6179c33ed37
ding-hai <dhairoot@gmail.com>
2021-03-23 18:03:22 +0800

    all APIs of trier

---
+++ src/main/java/com/dinghai/tree/Trier.java
@@ -0,0 +1,29 @@
+package com.dinghai.tree;
+
+import java.util.List;
+
+public class Trier {
+
+    private class Node{
+        boolean wordFlag;
+        Node[] children;
+    }
+
+    public Trier(){
+
+    }
+
+    public void add(String s){
+        //TODO
+    }
+
+    public boolean exists(String s){
+        //TODO
+        return false;
+    }
+
+    public List<String> startWith(String prefix){
+        //TODO
+        return null;
+    }
+}
